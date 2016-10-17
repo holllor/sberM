454ac7f1a8fdd58597cb8e9641eb98727ea2e678
олег <@zilberman>
2016-10-17 10:01:54 +0300

    первый тест

--- sberm2/src/main/java/ru/home/sberm2/MainTestA.java
+++ sberm2/src/main/java/ru/home/sberm2/MainTestA.java
@@ -5,10 +5,36 @@
  */
 package ru.home.sberm2;
 
+import com.opencsv.CSVReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author oleg
  */
 public class MainTestA {
-    
+    private String path = "C:\\table\\";
+
+    public static void main(String[] args){
+        MainTestA mm = new MainTestA();
+        try {
+            mm.ReaderCSV();
+        } catch (IOException ex) {
+            Logger.getLogger(MainTestA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void ReaderCSV() throws FileNotFoundException, IOException {
+        String filePath = path+"tr_mcc_codes.csv";
+        CSVReader reader = new CSVReader(new FileReader(filePath));
+        String[] nextLine;
+        while ((nextLine = reader.readNext()) != null) {
+            // nextLine[] is an array of values from the line
+            System.out.println(nextLine[0]);
+           // System.out.println(nextLine[0] + nextLine[1] + "etc... "+nextLine.length);
+        }
+    }
 }
