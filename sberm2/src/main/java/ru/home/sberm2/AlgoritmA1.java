c3f001ca014df3f1ec9733fc6b74ab7656ee5c5f
олег <@zilberman>
2016-10-20 15:46:40 +0300

    рабочий алгоритм А, дал 0.517 (это 280 место из 343) по улучшению смотри заметки!!

--- sberm2/src/main/java/ru/home/sberm2/AlgoritmA1.java
+++ sberm2/src/main/java/ru/home/sberm2/AlgoritmA1.java
@@ -5,6 +5,7 @@
  */
 package ru.home.sberm2;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -35,53 +36,56 @@ public class AlgoritmA1 {
         SpisNotGenderCustomJpaController jpa = new SpisNotGenderCustomJpaController(emf);
         listSpisNotGender = jpa.findSpisNotGenderCustomEntities();
         for (SpisNotGenderCustom spisNotGenderCustom : listSpisNotGender) {// перебираем всех неопределенных пользователей
-          
+
             listSrednNeopr = jpa.findSpisSrednNeoprEntities(); // выбор списка всех средних, коэф. и их мсс кодов  
             Integer id = spisNotGenderCustom.getCustomerId(); // получение ид пользоватлея
-           // System.out.println(id);
+            // System.out.println(id);
             List<List<String>> listTest = jpa.selectSrednCustomer(id);// выбираем по ид ключевые коды пользователя
             float genderCustomer = 0;// пол пользоватлея по умолчанию Ж
             // Проверка на случай если у пользователя нет ключевых кодов (они выбирались по наиболее сильным отличиям М и Ж)
-            
-            if (listTest == null || listTest.isEmpty() ) {// если нуль то вероятность = рандому, присваеваем случайное значение
+
+            if (listTest == null || listTest.isEmpty()) {// если нуль то вероятность = рандому, присваеваем случайное значение
                 Integer veroyatn = (int) (Math.random() + 0.5);// произвольное выпадение 0 или 1
                 genderCustomer = veroyatn.floatValue();
             } else {// иначе
                 List<Integer> veroyatAll = new ArrayList<>(); // список ключевых пользователя, для каждого из них вероятность разная (итог по среднему значению)
                 for (List<String> list : listTest) { // перебираем ключевые коды пользователя
-                   // System.out.println(list.get(0) + " " + list.get(1));
+                    // System.out.println(list.get(0) + " " + list.get(1));
                     int mcc_codeCust = Integer.valueOf(list.get(0));
                     int srednCust = Integer.valueOf(list.get(1));
 
                     for (SrednNeopr srednNeop : listSrednNeopr) { // перебор по таблице средний неопределенный(все ключ. значения)
                         if (srednNeop.getMccCode() == mcc_codeCust) { // выбираем конктреную строку из выборки
-                            
-                            float srNeopKoef = (float) (srednNeop.getSredn().floatValue() / 1.261); // допущение 1 муж больше
-                            System.out.println("kod = "+mcc_codeCust+ " value = "+srNeopKoef+" ? "+srednCust * srednNeop.getKoef());
+
+                            float srNeopKoef = (float) (srednNeop.getSredn().floatValue() ); // допущение 1 ( / 1.261)муж больше
+                            // System.out.println("kod = "+mcc_codeCust+ " value = "+srNeopKoef+" ? "+srednCust * srednNeop.getKoef());
                             if (srNeopKoef <= srednCust * srednNeop.getKoef()) {// если среднее меньше то М
-                                Integer veroyat1 = 1;
-                                veroyatAll.add(veroyat1);
-                            } 
-                           if (srNeopKoef > srednCust * srednNeop.getKoef())  {// если нет то Ж
-                                Integer veroyat2 = 0;
-                                System.out.println("Ж");
-                                veroyatAll.add(veroyat2);
+
+                                veroyatAll.add(1);// М
+                            }
+                            if (srNeopKoef > srednCust * srednNeop.getKoef()) {// если нет то Ж
+
+                       //         System.out.println("Ж");
+                                veroyatAll.add(0); //Ж
                             }
                         }
                     }
                 }
                 int itog = 0;
                 for (int i = 0; i < veroyatAll.size(); i++) {
-                    itog += veroyatAll.get(0);
+                    //  System.out.println("i "+veroyatAll.get(0));
+                    itog += veroyatAll.get(i);
                 }
-               // System.out.println(id+" "+veroyatAll.size());
-              // System.out.println(itog+" "+veroyatAll.size());
-                float verItog = itog / veroyatAll.size();
-                
+                // System.out.println(id+" "+veroyatAll.size());
+              //  System.out.println(itog + " " + veroyatAll.size());
+                float verItog = (float) itog / (float) veroyatAll.size();
+                //    System.out.println("veroyat= "+verItog); 
                 genderCustomer = verItog;
             }
-            System.out.println(id +" "+genderCustomer);
-        } 
-        
+            NumberFormat nf = NumberFormat.getInstance();//1 знако после запятой
+            nf.setMaximumFractionDigits(1);
+            System.out.println(id + ";" + nf.format(genderCustomer));
+        }
+
     }
 }
